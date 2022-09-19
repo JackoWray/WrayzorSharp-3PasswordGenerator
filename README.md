ea655618088d5332355923b89738b67178089f41
Jackson Wray <jackson.wray19@outlook.com>
2022-09-19 17:19:23 +0800

    Complete, no errors, satisfies criteria, read me todo

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # WrayzorSharp-3PasswordGenerator
+
+💡The goal was to create an application for the user to generate a unique secure password. The application is to allow customisation to the potential password in the way of variable length and being able to select and deselect differnet character groups.
+
+The stater index.html and script.js were both edited and added to extensively, firstly assigning separate character groups to arrays as variables to allow access to each type separately; coding a form for the password parameters (length and character types); effectively referencing the form inputs in the script.js and assigning as variables icluding a resultant array concatenating arrays from all selected in the form; and finally a for-loop generating the password to be printed to the user.
+
+![Webpage Image](<./assets/images/Screenshot-(5).png>)
+![Webpage Image](<./assets/images/Screenshot-(6).png>)
+
+To access the deployed code, please click on the following link:
+[WrayzorSharpGitHubPage](https://jackowray.github.io/WrayzorSharp-3PasswordGenerator/ "Wrayzor Sharp Password Generator Application")
+
+Enjoy! 😄
--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,3 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
 var charsLcaseString = "qwertyuiopasdfghjklzxcvbnm";
 var charsLcaseArr = charsLcaseString.split("");
 var charsUcaseString = "QWERTYUIOPASDFGHJKLZXCVBNM";
@@ -7,14 +5,69 @@ var charsUcaseArr = charsUcaseString.split("");
 var charsNumber = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var charsSpec = " !'()*+,-./:;<=>?@[^_`{|}~";
 var charsSpecArr = charsSpec.split("");
+const generatePWBtn = document.getElementById("generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = function generatePassword() {};
-  var passwordText = document.querySelector("#password");
+function askPrompts() {
+  let password = "";
+  let charsSelected = [];
+  let pwLength = prompt(
+    "How many characters wound you like your password to be? (Enter a number between 8 and 128.)"
+  );
+  if (isNaN(pwLength) == true) {
+    alert("Please ensure input is a number.");
+  } else {
+    if (parseInt(pwLength) > 7 && parseInt(pwLength) < 129) {
+      let includeLC = confirm(
+        "Would you like your password to potentially include lowercase letters? (OK for yes, Cancel for no.)"
+      );
+      if (includeLC == true) {
+        for (let i = 0; i < charsLcaseArr.length; i++) {
+          charsSelected.push(charsLcaseArr[i]);
+        }
+      }
 
-  passwordText.value = password;
+      let includeUC = confirm(
+        "Would you like your password to potentially include uppercase letters? (OK for yes, Cancel for no.)"
+      );
+      if (includeUC == true) {
+        for (let i = 0; i < charsUcaseArr.length; i++) {
+          charsSelected.push(charsUcaseArr[i]);
+        }
+      }
+
+      let includeNumbers = confirm(
+        "Would you like your password to potentially include numerical characters? (OK for yes, Cancel for no.)"
+      );
+      if (includeNumbers == true) {
+        for (let i = 0; i < charsNumber.length; i++) {
+          charsSelected.push(charsNumber[i]);
+        }
+      }
+
+      let includeSpec = confirm(
+        "Would you like your password to potentially include special characters (i.e. '!' or '@')? (OK for yes, Cancel for no.)"
+      );
+      if (includeSpec == true) {
+        for (let i = 0; i < charsSpecArr.length; i++) {
+          charsSelected.push(charsSpecArr[i]);
+        }
+      }
+    } else {
+      alert("Please ensure input is between 8 and 128.");
+    }
+  }
+
+  // console.log(charsSelected);
+
+  function generatePassword() {
+    for (var i = 0; i < parseInt(pwLength); i++) {
+      var randomNumber = Math.floor(Math.random() * charsSelected.length);
+      password += charsSelected[randomNumber];
+    }
+    return password;
+  }
+  const passwordText = document.getElementById("password");
+
+  passwordText.innerHTML = generatePassword();
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generatePWBtn.addEventListener("click", askPrompts);
